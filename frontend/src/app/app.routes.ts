e27367c3d01dd9a4bba7faf46314f0d2de834e4f
Daniel Hufschlaeger <daniel@hufschlaeger.net>
2026-05-19 16:01:09 +0200

    Modified default route.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login';
-import { DashboardComponent } from './components/dashboard/dashboard';
+
 import { authGuard } from './auth/auth-guard';
+import {LayoutComponent} from './components/layout/layout.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   {
     path: 'dashboard',
-    component: DashboardComponent,
+    component: LayoutComponent,
     canActivate: [authGuard]
   },
   { path: '**', redirectTo: '/dashboard' }
